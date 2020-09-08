6da2ac6ab64a7e234a18ea8cd900957a1dd56164
shrey <nshrey53@gmail.com>
2020-09-08 09:16:44 +0545

    active navitem scrollbar file added, but PROBLEM

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,43 @@
 $(document).ready(function () {
+  function onScroll(event){
+    console.log('am i being called')
+    var scrollPos = $(document).scrollTop();
+    $('#navbarNav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#navbarNav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
+$(document).on("scroll", onScroll);
+    
+//smoothscroll
+$('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    $(document).off("scroll");
+    
+    $('a').each(function () {
+        $(this).removeClass('active');
+    })
+    $(this).addClass('active');
+  
+    var target = this.hash,
+        menu = target;
+    $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top+2
+    }, 500, 'swing', function () {
+        window.location.hash = target;
+        $(document).on("scroll", onScroll);
+    });
+});
+
+
   const nav_offset_top = $(".header_area").height() + 300;
   function navbarFixed() {
     if ($(".header_area").length) {
@@ -31,3 +70,6 @@ $(document).ready(function () {
 
   navbarFixed();
 });
+
+
+
